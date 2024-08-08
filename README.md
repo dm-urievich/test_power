2bf75ce2298239957d37e70ce228a4702ffaa5ba
Dmytro Rezchenko <dm.urievich@gmail.com>
2024-08-08 09:00:09 +0300

    added more targets, analog output to control amplification

--- README.md
+++ README.md
@@ -4,7 +4,9 @@ disable carrier<br>
 cw 0<br>
 <br>
 set output frequency<br>
-freq 868<br>
+f 868<br>
 <br>
-set output power<br>
-pwr 10<br>
+set output power, value will be passed to radiolib<br>
+p 10<br>
+set analog output (usually to control amplification), values from 0 to 255<br>
+a 127<br>
--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,18 @@
 #include <SPI.h>
 #include <RadioLib.h>
 
-#define TTGO_V2 1
+//#define TTGO_V2 1
+// #define BARVINOK_TX 1
+// #define RADIOMATER_BANDIT 1
+#define ES900TX 1
+// #define BETAFPV_MICRO 1
+// #define ELRS_RX 1
+// #define BETAFPV_TX_400 1
+
+
+// #define CW_FROM_STARTUP 1
+float default_freq = 750;
+float default_pwr = 2;
 
 #if defined(TTGO_V2)
 
@@ -18,14 +29,66 @@
 
 #define LED_BUILTIN 25
 
-#else
+const bool radio_rfo_hf = false;
 
-/*    "radio_dio0": 4,
-    "radio_miso": 19,
-    "radio_mosi": 23,
-    "radio_nss": 5,
-    "radio_rst": 14,
-    "radio_sck": 18,*/
+#elif defined(BARVINOK_TX)
+
+// Barvinok ES900 SL Retrick
+#define LORA_CS     27
+#define LORA_IRQ    36
+#define LORA_RST    26
+
+
+//right chip
+#define LORA_CS_2     13
+#define LORA_IRQ_2    39
+#define LORA_RST_2    21
+
+#define LORA_SCK    25
+#define LORA_MISO   33
+#define LORA_MOSI   32
+
+#define LED_BUILTIN 22
+
+const bool radio_rfo_hf = false;
+
+#elif defined(RADIOMATER_BANDIT)
+
+#define LORA_CS     4
+#define LORA_IRQ    22
+#define LORA_RST    5
+
+#define LORA_SCK    18
+#define LORA_MISO   19
+#define LORA_MOSI   23
+
+#define LED_BUILTIN 15
+
+const bool radio_rfo_hf = true;
+
+#define TX_EN_PIN 33
+#define FAN_EN_PIN 2
+
+#elif defined(ES900TX)
+
+#define LORA_CS     5
+#define LORA_IRQ    26
+#define LORA_RST    14
+
+#define LORA_SCK    18
+#define LORA_MISO   19
+#define LORA_MOSI   23
+
+#define LED_BUILTIN 15
+
+const bool radio_rfo_hf = false;
+
+#define TX_EN_PIN 12
+#define FAN_EN_PIN 17
+#define PIN_RFamp_APC2 25
+
+#elif defined(BETAFPV_MICRO)
+
 #define LORA_CS     5
 #define LORA_IRQ    4
 #define LORA_RST    14
@@ -34,13 +97,50 @@
 #define LORA_MISO   19
 #define LORA_MOSI   23
 
-#endif 
+#define LED_BUILTIN 15
+
+const bool radio_rfo_hf = true;
+
+#define TX_EN_PIN 26
+#define FAN_EN_PIN 17
+
+#elif defined(ELRS_RX)
+
+#define LORA_CS     15
+#define LORA_IRQ    4
+#define LORA_RST    2
+
+#define LORA_SCK    14
+#define LORA_MISO   12
+#define LORA_MOSI   13
+
+#define LED_BUILTIN 16
+
+const bool radio_rfo_hf = false;
+
+#elif defined(BETAFPV_TX_400)
+
+#define LORA_CS     2
+#define LORA_IRQ    34
+#define LORA_RST    13
+
+#define LORA_SCK    25
+#define LORA_MISO   33
+#define LORA_MOSI   32
+
+#define LED_BUILTIN 15
+
+const bool radio_rfo_hf = false;
+
+#define FAN_EN_PIN 27
+
+#endif
 
 #include "cli.h"
 #include <string.h>
 
 #define CLI_BUFFER_SIZE 128
-#define CLI_COMMAND_COUNT 4
+#define CLI_COMMAND_COUNT 5
 
 #define CLI_ENTRY(command) { command ## Callback, (const CLI_BUF_VALUE_T *) #command } 
 
@@ -48,9 +148,10 @@ static void cliRxCallback(uint8_t data);
 static void cliTxCallback(CLI_BUF_VALUE_T *buf, CLI_TX_BUF_COUNT_VALUE_T bufc);
 
 static CLIRet_t cliCallback(void *args, CLI_ARG_COUNT_VALUE_T argc);
-static CLIRet_t pwrCallback(void *args, CLI_ARG_COUNT_VALUE_T argc);
-static CLIRet_t freqCallback(void *args, CLI_ARG_COUNT_VALUE_T argc);
+static CLIRet_t pCallback(void *args, CLI_ARG_COUNT_VALUE_T argc);
+static CLIRet_t fCallback(void *args, CLI_ARG_COUNT_VALUE_T argc);
 static CLIRet_t cwCallback(void *args, CLI_ARG_COUNT_VALUE_T argc);
+static CLIRet_t aCallback(void *args, CLI_ARG_COUNT_VALUE_T argc);
 
 CLIConfig_t l_cli_cnf;
 CLIInst_t l_cli_inst;
@@ -58,20 +159,25 @@ CLI_BUF_VALUE_T l_cli_buf[CLI_BUFFER_SIZE];
 CLICommand_t l_cli_commands[CLI_COMMAND_COUNT] =
 {
     CLI_ENTRY(cli),
-    CLI_ENTRY(pwr),
-    CLI_ENTRY(freq),
+    CLI_ENTRY(p),
+    CLI_ENTRY(f),
     CLI_ENTRY(cw),
+    CLI_ENTRY(a),
 };
-
-float default_freq = 915;
-float default_pwr = 1;
 
 SX1276 radio = new Module(LORA_CS, LORA_IRQ, LORA_RST);
 
+//SX1276 radio_2 = new Module(LORA_CS_2, LORA_IRQ_2, LORA_RST_2);
+
 void initLoRa() {
     Serial.println("Initializing LoRa....");
-    SPI.begin(LORA_SCK, LORA_MISO, LORA_MOSI, LORA_CS);
 
+#if not defined(ELRS_RX)
+    SPI.begin(LORA_SCK, LORA_MISO, LORA_MOSI, LORA_CS);
+#else
+    SPI.pins(LORA_SCK, LORA_MISO, LORA_MOSI, LORA_CS);
+    SPI.begin();
+#endif
     int state = radio.beginFSK();
     if (state == RADIOLIB_ERR_NONE) {
         Serial.println(F("success!"));
@@ -89,13 +195,27 @@ void initLoRa() {
     // the following settings can also
     // be modified at run-time
     state = radio.setFrequency(default_freq);
-    state = radio.setOutputPower(default_pwr);
+    state = radio.setOutputPower(default_pwr, radio_rfo_hf);
 
     // state = radio.transmitDirect();
     // if (state != RADIOLIB_ERR_NONE) {
     //     Serial.println(F("[SX1278] Unable to start direct transmission mode, code "));
     //     Serial.println(state);
     // }
+
+    #if defined(CW_FROM_STARTUP)
+        state = radio.transmitDirect();
+
+        if (state != RADIOLIB_ERR_NONE) {
+            Serial.println(F("[SX1278] Unable to start direct transmission mode, code "));
+            Serial.println(state);
+        }
+        else {
+            Serial.println("CW ON");
+            digitalWrite(LED_BUILTIN, HIGH);
+        }
+    #endif
+
 }
 
 void setup() {
@@ -104,10 +224,9 @@ void setup() {
 
     Serial.println("Setup test power tool....");
 
-    initLoRa();
-    
     pinMode(LED_BUILTIN, OUTPUT);
 
+    initLoRa();
 
     l_cli_cnf.buf = l_cli_buf;
     l_cli_cnf.bufc = CLI_BUFFER_SIZE;
@@ -117,19 +236,36 @@ void setup() {
 
     CLIInit(&l_cli_inst, l_cli_cnf);
 
+
+    #if defined(TX_EN_PIN)
+        pinMode(TX_EN_PIN, OUTPUT);
+        digitalWrite(TX_EN_PIN, HIGH);
+    #endif
+
+    #if defined(FAN_EN_PIN)
+        pinMode(FAN_EN_PIN, OUTPUT);
+        digitalWrite(FAN_EN_PIN, HIGH);
+    #endif
+
+    #if defined(PIN_RFamp_APC2)
+        dacWrite(PIN_RFamp_APC2, 30);
+    #endif
+
 }
 
 void loop() {
     // Check if user typed something into the serial monitor
     while (Serial.available()) {
-       cliRxCallback(Serial.read());
+        uint8_t a;
+        a = Serial.read();
+       cliRxCallback(a);
     }
 
     CLIHandle(&l_cli_inst);
 }
 
 
-static CLIRet_t pwrCallback(void *args, CLI_ARG_COUNT_VALUE_T argc)
+static CLIRet_t pCallback(void *args, CLI_ARG_COUNT_VALUE_T argc)
 {
     void *buf = NULL;
     CLIArg_t arg = {0U}; 
@@ -139,7 +275,7 @@ static CLIRet_t pwrCallback(void *args, CLI_ARG_COUNT_VALUE_T argc)
     if (buf) {
         int pwr = atoi((char*)buf);
 
-        int state = radio.setOutputPower(pwr);
+        int state = radio.setOutputPower(pwr, radio_rfo_hf);
         if (state != RADIOLIB_ERR_NONE) {
             Serial.println(F("Selected output power is invalid for this module!"));
             Serial.println(state);
@@ -157,7 +293,7 @@ static CLIRet_t pwrCallback(void *args, CLI_ARG_COUNT_VALUE_T argc)
 }
 
 
-static CLIRet_t freqCallback(void *args, CLI_ARG_COUNT_VALUE_T argc)
+static CLIRet_t fCallback(void *args, CLI_ARG_COUNT_VALUE_T argc)
 {
     void *buf = NULL;
     CLIArg_t arg = {0U}; 
@@ -182,6 +318,26 @@ static CLIRet_t freqCallback(void *args, CLI_ARG_COUNT_VALUE_T argc)
 	return CLI_OK;
 }
 
+static CLIRet_t aCallback(void *args, CLI_ARG_COUNT_VALUE_T argc)
+{
+    void *buf = NULL;
+    CLIArg_t arg = {0U}; 
+
+    buf = CLIArgParse(&l_cli_inst, &arg, args, argc);
+
+    if (buf) {
+        int apc = atoi((char*)buf);
+
+        dacWrite(PIN_RFamp_APC2, apc);
+
+        Serial.printf("Set DAC power control %d", apc);
+    }
+    else {
+        Serial.println("please set dac power control <dac value>");
+    }
+	return CLI_OK;
+}
+
 static CLIRet_t cwCallback(void *args, CLI_ARG_COUNT_VALUE_T argc)
 {
     void *buf = NULL;
@@ -198,6 +354,7 @@ static CLIRet_t cwCallback(void *args, CLI_ARG_COUNT_VALUE_T argc)
         }
         else {
             int state = radio.transmitDirect();
+
             if (state != RADIOLIB_ERR_NONE) {
                 Serial.println(F("[SX1278] Unable to start direct transmission mode, code "));
                 Serial.println(state);
